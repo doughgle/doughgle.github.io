8f1b91ad1b9bdcde84f4a06fda9bafbbd58f5362
Douglas Hellinger <doug.hellinger@hotmail.com>
2014-03-25 12:26:43 +0800

    removed game of life, added Yose answer.

--- README.md
+++
@@ -1,4 +0,0 @@
-game_of_life_web
-================
-
-Conway's game of life in your browser. Fun, fun, fun.
--- src-test/cellTest.js
+++
@@ -1,6 +0,0 @@
-CellTest = TestCase("CellTest");
-
-
-//CellTest.prototype.test_nothing = function() {
-//  assertTrue(false);
-//};
